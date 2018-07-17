cd41cba7125ad9fe2ee0dab3cb32aef4323af966
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-17 02:32:05 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 
 def reverse_each_word(sentence)
-  my_sentence = ["Hello there, and how are you?"]
+  sentence = "Hello there, and how are you?"
   sentence.collect do |word|
-    my_sentence <<  word.reverse
+      word.reverse
 end
-my_sentence
 end
