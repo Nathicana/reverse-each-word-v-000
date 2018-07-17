502ff0765acc08186e9e937e55f55d5f9ad00d46
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-17 02:28:16 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 def reverse_each_word(sentence)
   my_sentence = []
   sentence.collect do |word|
-    my_sentence << puts "#{word.reverse}"
+    my_sentence <<  "#{word.reverse}"
 end
 my_sentence
 end
