3b03b0a9b5783d03e25e3f2ac436e8499041e84b
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-17 02:48:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   sentence = sentence.split
   my_new_sentence = []
   sentence.collect do |word|
-    my_new_sentence = word.reverse
+    my_new_sentence << word.reverse
      
 end
 my_new_sentence
